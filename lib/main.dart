6e01a70935debb758a3411e944d55cb0fe2982d8
Vincent Tirgei <tirgeic@gmail.com>
2020-04-20 11:28:09 +0300

    Remove debug ribbon

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:covid19/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     theme: ThemeData(fontFamily: 'Nunito'),
     home: HomeScreen(),
 ));
